ad987c9eaaeb1d38c5886eb6ec34a995d38960bd
wfacciolla2 <facciollacorp@gmail.com>
2021-05-18 21:57:44 -0300

    Adicionado: Balance for bank

--- JAVA/banco2/src/banco2/Account.java
+++ JAVA/banco2/src/banco2/Account.java
@@ -4,12 +4,33 @@ public class Account {
 
 	private String name;
 	public String getName;
+	private double balance;
+	
+	public Account(String name, double balance){
+		this.name = name;
+		
+		if(balance > 0.0)
+			this.balance = balance;
+	}
+	
+	public void deposit(double depositAmount){
+		if(depositAmount > 0.0)
+			balance = balance + depositAmount;
+	}
 	
 	public void setName(String name){
 		this.name = name;
 	}
 	public String getName() {
 		return name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
 	}
 
 }
