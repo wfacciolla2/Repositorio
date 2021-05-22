c0ab22ed15c36b31324e4e7deb41f6eceab2d82d
wfacciolla2 <facciollacorp@gmail.com>
2021-05-22 12:41:40 -0300

    Commitions in Portuguese

---
+++ JAVA/Polimorfismo/src/Polimorfismo/ControleDeBonificacoes.java
@@ -0,0 +1,15 @@
+package Polimorfismo;
+
+public class ControleDeBonificacoes {
+	
+	private double totalDeBonificacoes = 0;
+	
+	public void registra(Funcionario funcionario){
+		this.totalDeBonificacoes += funcionario.getBonificacao();
+	}
+	
+	public double getTotalDeBonificacoes(){
+		return this.totalDeBonificacoes;
+	}
+	
+}
---
+++ JAVA/Polimorfismo/src/Polimorfismo/TestaGerente.java
@@ -0,0 +1,20 @@
+package Polimorfismo;
+
+public class TestaGerente {
+
+	public static void main(String[] args) {
+	    ControleDeBonificacoes controle = new ControleDeBonificacoes();
+
+	    Gerente funcionario1 = new Gerente();
+	    funcionario1.setSalario(5000.0);
+	    controle.registra(funcionario1);
+
+	    Funcionario funcionario2 = new Funcionario();
+	    funcionario2.setSalario(1000.0);
+	    controle.registra(funcionario2);
+
+	    System.out.println(controle.getTotalDeBonificacoes());
+
+	}
+
+}
