bfa80d725c39d451db140dbe1d4efc6f5fcfc296
wfacciolla2 <facciollacorp@gmail.com>
2021-06-07 10:25:57 -0300

    Atualização de verificação

---
+++ SemanaSantander/src/app/shared/models/stock-model.ts
@@ -0,0 +1,12 @@
+class Stock {
+
+  id: number = 0;
+  name: string = "";
+  price: number = 0;
+  minimum: number = 0;
+  exclusiveMinimum: boolean = false;
+  variation: number = 0;
+  date: string = '';
+}
+
+export default Stock;
