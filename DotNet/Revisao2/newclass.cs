c8bd7759a3116467d4f06a3e244516cdb63eac6e
wfacciolla2 <facciollacorp@gmail.com>
2021-06-09 21:01:12 -0300

    Adicionada Revisao2

---
+++ DotNet/Revisao2/newclass.cs
@@ -0,0 +1,13 @@
+namespace Revisao
+{
+    public class newclass
+    {
+       while(true){
+           string s = Console.ReadLine();
+           if(string.IsNullOrEmbpty(s)){
+               break;
+           }
+           Console.WriteLine(s);
+       } 
+    }
+}
