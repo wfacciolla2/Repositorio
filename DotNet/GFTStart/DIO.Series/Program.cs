70714a2ffebc95771f53e54a3684e69e27e574f8
wfacciolla2 <facciollacorp@gmail.com>
2021-06-13 20:57:06 -0300

    Aula 01

---
+++ DotNet/GFTStart/DIO.Series/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie newObjeto = new Serie():
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
